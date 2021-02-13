541148ac922618e99e033b53b062b0b62a0d6190
sooojungp96 <sooojungp96@gmail.com>
2021-02-14 00:33:10 +0900

    test

--- src/main/java/anno/MyInfo.java
+++ src/main/java/anno/MyInfo.java
@@ -1,10 +1,13 @@
 package anno;
 
+/**
+ * 어노테이션
+ * 추상 메서드 abstract 반환 타입만 있는 default 설정 가능**/
 public @interface MyInfo {
-    int age();
-    String name();
+    int age();// 원시 타입 데이터
+    String name();// 참조 타입 데이터
     String[] hobby();
-    TestType testType();// enum을 요소로 가질 수 있다.
+    TestType testType();// enum 타입데이터
 //    DateTime testDate();
 
 }
--- src/main/java/my_cafe/RunMy_cafe.java
+++ src/main/java/my_cafe/RunMy_cafe.java
@@ -2,6 +2,7 @@ package my_cafe;
 
 public class RunMy_cafe {
     public static void main(String[] args) {
+        System.out.println("testgit");
         NewbyGuest newbyGuest = new NewbyGuest(25);
         newbyGuest.payBill();
         newbyGuest.charge();
